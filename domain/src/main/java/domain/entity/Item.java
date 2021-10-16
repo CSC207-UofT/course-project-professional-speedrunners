743d6945cfb10eef63db3fbc76ebe67b26af1335
Maddobun <leollaabbaa@gmail.com>
2021-10-16 15:54:57 -0400

    Add documentations

--- domain/src/main/java/domain/entity/Item.java
+++ domain/src/main/java/domain/entity/Item.java
@@ -1,12 +1,24 @@
 package domain.entity;
 
+/**
+ * Class that represents an Item in the domain layer
+ */
 public class Item implements PriceComparable{
 
+    //TODO: store store object instead of storeID?
     private float price;
     private final String storeId;
     private final String id;
-    private final String name;
+    private String name;
 
+    /**
+     * Creates an Item with relevant parameters
+     * storeId, id should be final upon creation
+     * @param price the price of an PriceComparable object
+     * @param storeId id of the store to which this item is associated with.
+     * @param id unique id of the item
+     * @param name name of the item
+     */
     public Item(float price, String storeId, String id, String name){
         this.price = price;
         this.storeId = storeId;
@@ -30,16 +42,43 @@ public class Item implements PriceComparable{
         return name;
     }
 
+    //TODO: add this setter into the PriceCOmparable interface
+    /**
+     * setter for Price. Mutating method
+     * @param price new price of the item
+     */
     public void setPrice(float price) {
         this.price = price;
     }
 
+    /**
+     * setter for Price. Mutating method
+     * @param name new name of the item
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * generates a String representation of the object.
+     * This should include all fields represented in string format.
+     * Currently, an item usecase returns a query response by adding the toString of the item to the response
+     * since ResponseModel objects can only carry Strings at the moment.
+     * This behaviour may change in the future.
+     * @return String representation of the item
+     */
     @Override
     public String toString(){
         return "Item: " + this.name + ", Price: " + this.price + ", Id: " + this.id;
     }
 
-    //TODO: Implement toString for entity classes
+    /**
+     * A builder class for item objects
+     * This class is responsible for making construction of objects more readable in code, among other benefits.
+     * It is not a necessary or important part of the class, but something nice to have.
+     * Refer to builder design patterns for more detail
+     * @return An ItemBuilder object
+     */
     public static ItemBuilder builder(){return new ItemBuilder();}
 
     public static class ItemBuilder{
