11d4f357e488c74bf4fe7c2f5c13572711e3ac6d
Angela <angela.wei@mail.utoronto.ca>
2021-10-31 12:54:57 -0400

    Added classes for store usecase (unfinished)

---
+++ src/main/java/com/boba/bobabuddy/core/usecase/store/CreateStore.java
@@ -0,0 +1,11 @@
+package com.boba.bobabuddy.core.usecase.store;
+
+import com.boba.bobabuddy.core.entity.Store;
+import com.boba.bobabuddy.core.usecase.port.storeport.ICreateStore;
+
+public class CreateStore implements ICreateStore {
+    @Override
+    public Store create(Store store) {
+        return null;
+    }
+}
