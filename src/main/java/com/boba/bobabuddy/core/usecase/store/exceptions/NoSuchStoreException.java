1c9230c7ade921017469ad99f19b7d2c117cae0e
lebronjames0991 <77180822+lebronjames0991@users.noreply.github.com>
2021-11-01 22:02:45 -0400

    exception for updatestore

---
+++ src/main/java/com/boba/bobabuddy/core/usecase/store/exceptions/NoSuchStoreException.java
@@ -0,0 +1,7 @@
+package com.boba.bobabuddy.core.usecase.store.exceptions;
+
+public class NoSuchStoreException extends Exception{
+    public NoSuchStoreException(String msg, Throwable err) {
+        super(msg, err);
+    }
+}
