c0c0b4cf4c59ce2f7509ffcd0a5c5b1ad9b533ef
Maddobun <leollaabbaa@gmail.com>
2021-11-04 17:40:46 -0400

    Update user related operations

--- src/main/java/com/boba/bobabuddy/core/usecase/port/request/LoginUserRequest.java
+++ src/main/java/com/boba/bobabuddy/core/usecase/port/request/LoginUserRequest.java
@@ -4,19 +4,19 @@ public class LoginUserRequest {
     String email;
     String password;
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getEmail() {
         return email;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
