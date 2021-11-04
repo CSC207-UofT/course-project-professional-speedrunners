f20a8c347044401c481cb84a0c162a562b6e3dbf
Arup Ukil <41102781+ArupUkil@users.noreply.github.com>
2021-11-03 20:01:02 -0400

    Forgot some files which include the exception that catches when a user is already in the repo and the LoginUserRequest.

---
+++ src/main/java/com/boba/bobabuddy/core/usecase/port/request/LoginUserRequest.java
@@ -0,0 +1,22 @@
+package com.boba.bobabuddy.core.usecase.port.request;
+
+public class LoginUserRequest {
+    String email;
+    String password;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
