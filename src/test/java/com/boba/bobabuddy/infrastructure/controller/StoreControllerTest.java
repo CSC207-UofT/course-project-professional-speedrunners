e07336e919fb4a3c9c13fc10e27f14bcb95ba6f5
Angela <angela.wei@mail.utoronto.ca>
2021-11-15 17:38:27 -0500

    Completed tests for store controller (createStore and findStore methods) - Angela

--- src/test/java/com/boba/bobabuddy/infrastructure/controller/StoreControllerTest.java
+++ src/test/java/com/boba/bobabuddy/infrastructure/controller/StoreControllerTest.java
@@ -13,28 +13,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -66,35 +59,76 @@ public class StoreControllerTest {
     @Autowired
     private ObjectMapper modelMapper;
 
-    private Store store;
+    private Store store1, store2, store3;
+    private Rating rating;
+    private Item item;
     private StoreDto createStoreRequest;
-    private List<Store> allStore;
-    private UUID id, id1;
+    private List<Store> allStoreLst, ratingLst, nameLst;
+    private UUID id1, id2, id3, ratingId, itemId;
 
     @BeforeEach
     void setUp() {
-        store = new Store("Blobs", "bloor");
-        id = UUID.randomUUID();
-        store.setId(id);
+        store1 = new Store("Blobs", "bloor");
+        store2 = new Store("T", "bloor");
+        store3 = new Store("Chatime", ":)");
+
+        id1 = UUID.randomUUID();
+        id2 = UUID.randomUUID();
+        id3 = UUID.randomUUID();
+        store1.setId(id1);
+        store2.setId(id2);
+        store3.setId(id3);
+
+        store1.setAvgRating(1);
+        store2.setAvgRating(0.5f);
+        store3.setAvgRating(0.4f);
+
+        item = new Item(2.0f, store1, "yum");
+        itemId = UUID.randomUUID();
+        item.setId(itemId);
+        store1.addItem(item);
+
+        rating = new Rating(1, null, store1);
+        ratingId = UUID.randomUUID();
+        rating.setId(ratingId);
+        store1.addRating(rating);
 
         createStoreRequest = new StoreDto();
         createStoreRequest.setName("mmm");
         createStoreRequest.setLocation("map");
 
-        id1 = UUID.randomUUID();
-
-        allStore = Arrays.asList(store);
+        allStoreLst = Arrays.asList(store1, store2, store3);
+        ratingLst = Arrays.asList(store1, store2);
+        nameLst = List.of(store1);
     }
 
     @AfterEach
     void teardown() {
-        store = null;
+        store1 = null;
+        store2 = null;
+        store3 = null;
+
+        id1 = null;
+        id2 = null;
+        id3 = null;
+
+        item = null;
+        itemId = null;
+
+        rating = null;
+        ratingId = null;
+
+        createStoreRequest = null;
+
+        allStoreLst = null;
+        ratingLst = null;
+        nameLst = null;
     }
 
     @Test
     void createStore() throws Exception{
         Store createdStore = new Store("mmm", "map");
-        createdStore.setId(id);
+        createdStore.setId(id1);
         when(createStore.create(ArgumentMatchers.isA(Store.class))).thenReturn(createdStore);
 
         mockMvc.perform(post("/stores")
@@ -104,56 +138,129 @@ public class StoreControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
 
-                .andExpect(jsonPath("$.id", is(id.toString())))
+                .andExpect(jsonPath("$.id", is(id1.toString())))
                 .andExpect(jsonPath("$.name", is("mmm")))
                 .andExpect(jsonPath("$.location", is("map")));
     }
 
     @Test
     void findAll() throws Exception {
-        when(findStore.findAll()).thenReturn(allStore);
+        when(findStore.findAll()).thenReturn(allStoreLst);
 
         mockMvc.perform(get("/stores"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaTypes.HAL_JSON))
 
-                .andExpect(jsonPath("$._embedded.stores.[0].id", is(id.toString())))
+                .andExpect(jsonPath("$._embedded.stores.[0].id", is(id1.toString())))
+                .andExpect(jsonPath("$._embedded.stores.[0].name", is("Blobs")))
+                .andExpect(jsonPath("$._embedded.stores.[0].location", is("bloor")))
+                .andExpect(jsonPath("$._embedded.stores.[1].id", is(id2.toString())))
+                .andExpect(jsonPath("$._embedded.stores.[1].name", is("T")))
+                .andExpect(jsonPath("$._embedded.stores.[1].location", is("bloor")))
+                .andExpect(jsonPath("$._embedded.stores.[2].id", is(id3.toString())))
+                .andExpect(jsonPath("$._embedded.stores.[2].name", is("Chatime")))
+                .andExpect(jsonPath("$._embedded.stores.[2].location", is(":)")));
+    }
+
+    @Test
+    void findById() throws Exception {
+        when(findStore.findById(id1)).thenReturn(store1);
+
+        mockMvc.perform(get("/stores/{id}", id1.toString()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaTypes.HAL_JSON))
+
+                .andExpect(jsonPath("$.id", is(id1.toString())))
+                .andExpect(jsonPath("$.name", is("Blobs")))
+                .andExpect(jsonPath("$.location", is("bloor")));
+
+    }
+
+    @Test
+    void findByItem() throws Exception {
+        when(findStore.findByItem(eq(itemId))).thenReturn(store1);
+
+        mockMvc.perform(get("/stores/?itemId={itemId}", itemId.toString()))
+
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaTypes.HAL_JSON))
+
+                .andExpect(jsonPath("$.id", is(id1.toString())))
+                .andExpect(jsonPath("$.name", is("Blobs")))
+                .andExpect(jsonPath("$.location", is("bloor")));
+    }
+
+    @Test
+    void findByLocation() throws Exception {
+        when(findStore.findByLocation("bloor")).thenReturn(ratingLst);
+
+        mockMvc.perform(get("/stores/?location=bloor"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaTypes.HAL_JSON))
+
+                .andExpect(jsonPath("$._embedded.stores.[0].id", is(id1.toString())))
+                .andExpect(jsonPath("$._embedded.stores.[0].name", is("Blobs")))
+                .andExpect(jsonPath("$._embedded.stores.[0].location", is("bloor")))
+                .andExpect(jsonPath("$._embedded.stores.[1].id", is(id2.toString())))
+                .andExpect(jsonPath("$._embedded.stores.[1].name", is("T")))
+                .andExpect(jsonPath("$._embedded.stores.[1].location", is("bloor")));
+
+    }
+
+    @Test
+    void findByName() throws Exception {
+        when(findStore.findByName("Blobs")).thenReturn(nameLst);
+
+        mockMvc.perform(get("/stores/?name=Blobs"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaTypes.HAL_JSON))
+
+                .andExpect(jsonPath("$._embedded.stores.[0].id", is(id1.toString())))
                 .andExpect(jsonPath("$._embedded.stores.[0].name", is("Blobs")))
                 .andExpect(jsonPath("$._embedded.stores.[0].location", is("bloor")));
     }
 
     @Test
-    void findById(){
+    void findByNameContaining() throws Exception {
+        when(findStore.findByNameContaining("s")).thenReturn(nameLst);
 
+        mockMvc.perform(get("/stores/?name-contain=s"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaTypes.HAL_JSON))
+
+                .andExpect(jsonPath("$._embedded.stores.[0].id", is(id1.toString())))
+                .andExpect(jsonPath("$._embedded.stores.[0].name", is("Blobs")))
+                .andExpect(jsonPath("$._embedded.stores.[0].location", is("bloor")));
     }
 
     @Test
-    void findByItem(){
+    void findByAvgRatingGreaterThanEqual() throws Exception {
+        when(findStore.findByAvgRatingGreaterThanEqual(0.5f, true)).thenReturn(ratingLst);
 
+        mockMvc.perform(get("/stores/?rating-geq=0.5&sorted=true"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaTypes.HAL_JSON))
+
+                .andExpect(jsonPath("$._embedded.stores.[0].id", is(id1.toString())))
+                .andExpect(jsonPath("$._embedded.stores.[0].name", is("Blobs")))
+                .andExpect(jsonPath("$._embedded.stores.[0].location", is("bloor")))
+                .andExpect(jsonPath("$._embedded.stores.[1].id", is(id2.toString())))
+                .andExpect(jsonPath("$._embedded.stores.[1].name", is("T")))
+                .andExpect(jsonPath("$._embedded.stores.[1].location", is("bloor")));
     }
 
     @Test
-    void findByLocation(){
+    void findByRating() throws Exception {
+        when(findStore.findByRating(ratingId)).thenReturn(store1);
 
-    }
+        mockMvc.perform(get("/stores/?ratingId={ratingId}", ratingId.toString()))
 
-    @Test
-    void findByName(){
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaTypes.HAL_JSON))
 
-    }
-
-    @Test
-    void findByNameContaining(){
-
-    }
-
-    @Test
-    void findByAvgRatingGreaterThanEqual(){
-
-    }
-
-    @Test
-    void findByRating(){
+                .andExpect(jsonPath("$.id", is(id1.toString())))
+                .andExpect(jsonPath("$.name", is("Blobs")))
+                .andExpect(jsonPath("$.location", is("bloor")));
 
     }
 
