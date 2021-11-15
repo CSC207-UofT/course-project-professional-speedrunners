3ff289176c23d6a78bd0a2c4d50a51714a603f2b
lebronjames0991 <77180822+lebronjames0991@users.noreply.github.com>
2021-11-15 15:17:32 -0500

    StoreControllerTest

--- src/test/java/com/boba/bobabuddy/infrastructure/controller/StoreControllerTest.java
+++ src/test/java/com/boba/bobabuddy/infrastructure/controller/StoreControllerTest.java
@@ -1,4 +1,41 @@
 package com.boba.bobabuddy.infrastructure.controller;
 
+import com.boba.bobabuddy.core.entity.Item;
+import com.boba.bobabuddy.core.entity.Rating;
+import com.boba.bobabuddy.core.entity.Store;
+import com.boba.bobabuddy.core.usecase.store.port.ICreateStore;
+import com.boba.bobabuddy.core.usecase.store.port.IFindStore;
+import com.boba.bobabuddy.core.usecase.store.port.IRemoveStore;
+import com.boba.bobabuddy.core.usecase.store.port.IUpdateStore;
+import com.boba.bobabuddy.infrastructure.assembler.StoreResourceAssembler;
+import com.boba.bobabuddy.infrastructure.dto.StoreDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 public class StoreControllerTest {
 }
