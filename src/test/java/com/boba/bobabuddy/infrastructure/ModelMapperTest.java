0c5bf78f24a977352d6b0606fc0c31b273a00d0f
Maddobun <leollaabbaa@gmail.com>
2021-11-15 19:25:11 -0500

    add tests

--- src/test/java/com/boba/bobabuddy/infrastructure/ModelMapperTest.java
+++ src/test/java/com/boba/bobabuddy/infrastructure/ModelMapperTest.java
@@ -7,15 +7,17 @@ import com.boba.bobabuddy.core.entity.Store;
 import com.boba.bobabuddy.infrastructure.dto.RatableObjectDto;
 import com.boba.bobabuddy.infrastructure.dto.RatingDto;
 import com.boba.bobabuddy.infrastructure.dto.StoreDto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
+@ExtendWith(MockitoExtension.class)
 public class ModelMapperTest {
 
     private ModelMapper modelMapper;
@@ -35,7 +37,7 @@ public class ModelMapperTest {
         Rating rating1 = new Rating(1, null, store1);
 
         RatingDto ratingDto = modelMapper.map(rating1, RatingDto.class);
-        return;
+        Assertions.assertNotNull(ratingDto);
 
     }
 }
