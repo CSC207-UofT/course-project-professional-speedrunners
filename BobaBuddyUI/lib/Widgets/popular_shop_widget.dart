4b66167d3562f8f5f77d85a35a285b6cb4898f39
Mason <84557225+PondsFargo@users.noreply.github.com>
2021-11-18 16:30:43 -0500

    Code cleanup for popular shop widget

--- BobaBuddyUI/lib/Widgets/popular_shop_widget.dart
+++ BobaBuddyUI/lib/Widgets/popular_shop_widget.dart
@@ -17,12 +17,6 @@ class _PopularShops extends State<PopularShops> {
 
     Database db = Database();
 
-    //final List testData = [["Chatime", "667 some street"],["Some other resturaunt", "another address"],["another store","990 another street"]]; // This is used as example data. We will be using reading from a json for final implementation
-
-    //var stores = db.getStoreNames();
-    // print('as');
-    // print(stores);
-
     return Container(
       margin: const EdgeInsets.only(top: 16),
       child: SingleChildScrollView(
@@ -36,11 +30,8 @@ class _PopularShops extends State<PopularShops> {
                     future: db.getStoreNames(),
                     builder: (context, AsyncSnapshot snapshot) {
                       if (!snapshot.hasData) {
-                        print(snapshot.data);
                         return const Center(child: CircularProgressIndicator());
                       } else {
-                        print("9009090909");
-                        print(snapshot.data);
                         return ListView.builder(
                             shrinkWrap: true,
                             itemCount: snapshot.data.length >= 3
@@ -48,7 +39,7 @@ class _PopularShops extends State<PopularShops> {
                                 : snapshot.data.length,
                             scrollDirection: Axis.horizontal,
                             itemBuilder: (BuildContext context, int index) {
-                              return singleShop(
+                              return _buildSingleShop(
                                   context: context,
                                   imageSrc:
                                       'https://d1ralsognjng37.cloudfront.net/3586a06b-55c6-4370-a9b9-fe34ef34ad61.jpeg',
@@ -59,69 +50,16 @@ class _PopularShops extends State<PopularShops> {
                                   items: snapshot.data[index]['menu']);
                             });
                       }
-                    })
-
-                //         ListView.builder(
-                //         itemCount:testData.length, //todo set a limit for amount of popular shops to be displayed on homepage
-                //         shrinkWrap: true,scrollDirection: Axis.horizontal,itemBuilder: (context, index){
-                //     return singleShop(
-                //               context: context,
-                //               imageSrc: 'https://d1ralsognjng37.cloudfront.net/3586a06b-55c6-4370-a9b9-fe34ef34ad61.jpeg', //todo mock data
-                //               title: testData.elementAt(index).elementAt(0) ?? "",
-                //               address: testData.elementAt(index).elementAt(1) ?? ""
-                //             );
-                // })
-                )
+                    }))
           ],
         ),
-
-        // children: <Widget>[
-        //
-        //   Container(
-        //     margin:
-        //     const EdgeInsets.only(left: SPACEBETWEEN),
-        //     child: singleCategory(
-        //       image: 'images/img_6.png',
-        //       title: 'Some Store',
-        //       address: 'some address'
-        //
-        //     ),
-        //   ),
-        //   Container(
-        //     margin:
-        //     EdgeInsets.only(left:SPACEBETWEEN ),
-        //     child: singleCategory(
-        //         image: 'images/img_7.png',
-        //         title: "Chatime",
-        //         address: 'some address'
-        //     ),
-        //   ),
-        //   Container(
-        //     margin:
-        //     EdgeInsets.only(left: SPACEBETWEEN),
-        //     child: singleCategory(
-        //         image: 'images/img_8.png',
-        //         title: "Another Store",
-        //         address: 'some address'
-        //     ),
-        //   ),
-        //   Container(
-        //     margin: EdgeInsets.only(
-        //         left: SPACEBETWEEN,
-        //         right: SPACEBETWEEN),
-        //     child: singleCategory(
-        //         image: 'images/img_9.png',
-        //         title: "Soda",
-        //         address: 'some address'
-        //     ),
-        //   ),
-        // ],
       ),
     );
   }
 }
 
-Widget singleShop(
+///Widget builder for a single shop widget
+Widget _buildSingleShop(
     {required String imageSrc,
     required String title,
     required String address,
@@ -137,12 +75,10 @@ Widget singleShop(
     itemId = '';
   } else {
     itemId = items[0]["id"];
-    //itemId = '';
   }
 
   return InkWell(
     onTap: () {
-      print("Navigate to ${title} shop page");
       Navigator.push(
           context,
           MaterialPageRoute(
@@ -187,12 +123,7 @@ Widget singleShop(
                     height: 100,
                     fit: BoxFit.fitWidth, errorBuilder: (BuildContext context,
                         Object exception, StackTrace? stackTrace) {
-                  // Appropriate logging or analytics, e.g.
-                  // myAnalytics.recordError(
-                  //   'An error occurred loading "https://example.does.not.exist/image.jpg"',
-                  //   exception,
-                  //   stackTrace,
-                  // );
+                  //Error handling for image
                   return const Image(
                       image: AssetImage("assets/images/default-store.png"));
                 })),
