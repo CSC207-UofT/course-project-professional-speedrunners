0e0b17b4fcf320f998160945a0f895cc1a08a92a
Mason <84557225+PondsFargo@users.noreply.github.com>
2021-11-25 16:17:57 -0500

    Implemented Admin panel
    Added Admin Panel and supporting firebase infrastructure to support it.

---
+++ BobaBuddyUI/lib/Screens/admin_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AdminPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement AdminPage
+    return const Scaffold(
+      body: Center(
+        child: Text('Admin Page'),
+      ),
+    );
+  }
+}
