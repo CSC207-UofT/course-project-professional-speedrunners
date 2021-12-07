47765413410b2a8742b46507e18cdaf536405f6d
Mixiati Aerzuo <mixiati.aerzuo@mail.utoronto.ca>
2021-12-07 16:38:04 -0500

    fixed finditem, findstore, removeitem test

--- src/test/java/com/boba/bobabuddy/core/service/item/FindItemImplTest.java
+++ src/test/java/com/boba/bobabuddy/core/service/item/FindItemImplTest.java
@@ -1,5 +1,6 @@
 package com.boba.bobabuddy.core.service.item;
 
+import com.boba.bobabuddy.core.domain.Category;
 import com.boba.bobabuddy.core.domain.Item;
 import com.boba.bobabuddy.core.domain.Rating;
 import com.boba.bobabuddy.core.domain.Store;
@@ -15,10 +16,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Sort;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -28,6 +26,9 @@ public class FindItemImplTest {
 
     @Mock
     private ItemJpaRepository repo;
+
+    @Mock
+    private Sort sort;
 
     @Mock
     private Store store;
@@ -54,9 +55,10 @@ public class FindItemImplTest {
         UUID itemId2 = UUID.randomUUID();
         UUID itemId3 = UUID.randomUUID();
 
-        item1 = new Item(5, store, "milk tea");
-        Item item2 = new Item(7, store, "fruit tea");
-        Item item3 = new Item(9, store, "bubble tea");
+        Set<Category> set = new HashSet<>();
+        item1 = new Item(5, store, set);
+        Item item2 = new Item(7, store, set);
+        Item item3 = new Item(9, store, set);
 
 
         item1.setId(itemId1);
@@ -86,11 +88,11 @@ public class FindItemImplTest {
 
     @Test
     void testFindByStore() {
-        when(repo.findByStore_id(storeId)).thenReturn(itemLst);
+        when(repo.findByStore_id(storeId, sort)).thenReturn(itemLst);
         when(store.getId()).thenReturn(storeId);
 
 
-        List<Item> returnedItem = findItemImpl.findByStore(store.getId(), );
+        List<Item> returnedItem = findItemImpl.findByStore(store.getId(), sort);
         assertIterableEquals(itemLst, returnedItem);
         assertNotNull(returnedItem);
 
@@ -98,18 +100,18 @@ public class FindItemImplTest {
 
     @Test
     void testFindAll() {
-        when(repo.findAll()).thenReturn(itemLst);
+        when(repo.findAll(sort)).thenReturn(itemLst);
 
-        List<Item> returnedItem = findItemImpl.findAll();
+        List<Item> returnedItem = findItemImpl.findAll(sort);
         assertIterableEquals(itemLst, returnedItem);
         assertNotNull(returnedItem);
     }
 
     @Test
     void testFindByName() {
-        when(repo.findByNameIgnoreCase("milk tea", )).thenReturn(itemLstName);
+        when(repo.findByNameIgnoreCase("milk tea", sort)).thenReturn(itemLstName);
 
-        List<Item> returnedItem = findItemImpl.findByName("milk tea", );
+        List<Item> returnedItem = findItemImpl.findByName("milk tea", sort);
         assertIterableEquals(itemLstName, returnedItem);
         assertNotNull(returnedItem);
 
@@ -117,9 +119,9 @@ public class FindItemImplTest {
 
     @Test
     void testFindByNameContaining() {
-        when(repo.findByNameContainingIgnoreCase("tea")).thenReturn(itemLst);
+        when(repo.findByNameContainingIgnoreCase("tea", sort)).thenReturn(itemLst);
 
-        List<Item> returnedItem = findItemImpl.findByNameContaining("tea", );
+        List<Item> returnedItem = findItemImpl.findByNameContaining("tea", sort);
         assertIterableEquals(itemLst, returnedItem);
         assertNotNull(returnedItem);
 
@@ -151,7 +153,7 @@ public class FindItemImplTest {
     void testFindByPriceLessThanEqual() {
         when(repo.findByPriceLessThanEqual(10, Sort.by("price").ascending())).thenReturn(itemLst);
 
-        List<Item> returnedItem = findItemImpl.findByPriceLessThanEqual(10, , true);
+        List<Item> returnedItem = findItemImpl.findByPriceLessThanEqual(10,Sort.by("price").ascending());
         assertIterableEquals(itemLst, returnedItem);
         assertNotNull(returnedItem);
     }
@@ -160,7 +162,7 @@ public class FindItemImplTest {
     void testFindBy() {
         when(repo.findByAvgRatingGreaterThanEqual(0, Sort.by("avgRating").descending())).thenReturn(itemLstRating);
 
-        List<Item> returnedItem = findItemImpl.findByAvgRatingGreaterThanEqual(0, , true);
+        List<Item> returnedItem = findItemImpl.findByAvgRatingGreaterThanEqual(0, Sort.by("avgRating").descending());
         assertIterableEquals(itemLstRating, returnedItem);
         assertNotNull(returnedItem);
     }
