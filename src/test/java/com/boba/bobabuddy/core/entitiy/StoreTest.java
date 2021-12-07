def65d257fc0035b7afac223118c0e30041b1a0b
Angela <angela.wei@mail.utoronto.ca>
2021-12-07 14:52:12 -0500

    Fixed store, updateStore, removeStore tests

--- src/test/java/com/boba/bobabuddy/core/entitiy/StoreTest.java
+++ src/test/java/com/boba/bobabuddy/core/entitiy/StoreTest.java
@@ -2,54 +2,73 @@ package com.boba.bobabuddy.core.entitiy;
 
 import com.boba.bobabuddy.core.domain.Store;
 import com.boba.bobabuddy.core.domain.Item;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StoreTest {
+    private Store store;
+    private Item item;
+
+    private UUID storeId;
+
+    @BeforeEach
+    void setup(){
+        storeId = UUID.randomUUID();
+        store = new Store();
+        store.setId(storeId);
+        store.setName("M.Jordan's fav milk tea");
+        store.setLocation("75 Charles St, Toronto, Ontario M5S 1K9");
+        store.setOwner("Bob");
+
+        item = new Item();
+        item.setStore(store);
+    }
+
+    @AfterEach
+    void tearDown(){
+        store = null;
+        item = null;
+    }
 
     @Test
     void testGetMenu(){
-        Store store = new Store("M.Jordan's fav milk tea", "75 Charles St, Toronto, Ontario M5S 1K9");
-        Item item = new Item(5, store, "milk tea");
         store.addItem(item);
         assertEquals(1, store.getMenu().size());
     }
 
     @Test
     void testGetLocation(){
-        Store store1 = new Store("M.Jordan's fav milk tea", "75 Charles St, Toronto, Ontario M5S 1K9");
-        assertEquals("75 Charles St, Toronto, Ontario M5S 1K9", store1.getLocation());
+        assertEquals("75 Charles St, Toronto, Ontario M5S 1K9", store.getLocation());
     }
 
     @Test
     void testSetLocation(){
-        Store store1 = new Store("M.Jordan's fav milk tea", "75 Charles St, Toronto, Ontario M5S 1K9");
-        store1.setLocation("Mars");
-        assertEquals("Mars", store1.getLocation());
+        store.setLocation("Mars");
+        assertEquals("Mars", store.getLocation());
     }
 
     @Test
     void testAddItem(){
-        Store store1 = new Store("M.Jordan's fav milk tea", "75 Charles St, Toronto, Ontario M5S 1K9");
-        Item item1 = new Item(5, store1, "milk tea");
-        Item item2 = new Item(19, store1, "coconut milk tea");
-        store1.addItem(item1);
-        store1.addItem(item2);
-        assertTrue(store1.getMenu().contains(item1));
-        assertTrue(store1.getMenu().contains(item2));
+        Item item1 = new Item();
+        item1.setId(UUID.randomUUID());
+        Item item2 = new Item();
+        item2.setId(UUID.randomUUID());
+
+        store.addItem(item1);
+        store.addItem(item2);
+        assertTrue(store.getMenu().contains(item1));
+        assertTrue(store.getMenu().contains(item2));
     }
 
     @Test
     void testRemoveItem(){
-        Store store1 = new Store("M.Jordan's fav milk tea", "75 Charles St, Toronto, Ontario M5S 1K9");
-        Item item1 = new Item(5, store1, "milk tea");
-        Item item2 = new Item(19, store1, "coconut milk tea");
-        store1.addItem(item1);
-        store1.addItem(item2);
-        store1.removeItem(item2);
-        assertTrue(store1.getMenu().contains(item1));
-        assertFalse(store1.getMenu().contains(item2));
+        store.removeItem(item);
+        assertEquals(0, store.getMenu().size());
     }
 
 
